1cabd4b163f901a11b77523133ca89caf981515b
Sota6174 <t119141som110223@gmail.com>
2021-04-19 17:02:52 +0900

    [add] Basic_Python課題 1-B, 1-C, 1-D

---
+++ AOJ/01/01_B.py
@@ -0,0 +1,4 @@
+x = int(input()) ** 3
+print(x)
+# y = int(input())
+# print(y ** 3)
---
+++ AOJ/01/01_D.py
@@ -0,0 +1,6 @@
+import datetime
+S = datetime.timedelta(seconds=int(input()))
+print(S)
+
+# S = int(input())
+# print(S // 3600, (S % 3600) // 60, (S % 3600) % 60, sep=':')
