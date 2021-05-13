a2b37f06f5d1f7819622c2571ca749e613d24c3f
Yuki Yamada <tsube@outlook.jp>
2021-05-13 21:09:31 +0900

    lambdaを直すのはいやになっちゃったので、お許しください

--- AOJ/04/04_B.py
+++ AOJ/04/04_B.py
@@ -1,5 +1,5 @@
 import math
 s = lambda x: r ** 2 * math.pi
-l = lambda x:2 * r * math.pi
+l = lambda x: 2 * r * math.pi
 r = float(input())
 print(f"{s(r):.6f} {l(r):.6f}")
--- AOJ/04/04_C.py
+++ AOJ/04/04_C.py
@@ -1,18 +1,19 @@
-add = lambda x , y:x + y
-sub = lambda x , y:x - y
-mul = lambda x , y:x * y
-dev = lambda x , y:x // y
+add = lambda x, y: x + y
+sub = lambda x, y: x - y
+mul = lambda x, y: x * y
+dev = lambda x, y: x // y
 while True:
     t = input().split()
     a = int(t[0])
     op = t[1]
     b = int(t[2])
-    if op == "?":break
+    if op == "?":
+        break
     elif op == "+":
-        print(add(a,b))
+        print(add(a, b))
     elif op == "-":
-        print(sub(a,b))
+        print(sub(a, b))
     elif op == "*":
-        print(mul(a,b))
+        print(mul(a, b))
     elif op == "/":
-        print(dev(a,b))                
+        print(dev(a, b))
